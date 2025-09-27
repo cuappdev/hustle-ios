021639c4710c741f40a24eac2ecb4381c89023d4
Jay Z. <jidong.1272@gmail.com>
2025-09-26 21:53:45 -0400

    minor error again

--- Center/ContentView.swift
+++ Center/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Button("Sign out"){
-                try AuthManager.shared.signOut()
+                AuthManager.shared.signOut()
             }
         }
         .padding()
